5ea9191c38eaa3530c0823bae6b43001a3b83ac0
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-14 21:30:14 +0530

    [vinit]:add welcome

---
+++ src/main/java/com/bridgelabz/ConnectionDB.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class ConnectionDB {
+    String jdbcURL = "jdbc:mysql://localhost:3307/payroll_service?useSSL=false";
+    String username = "root";
+    String password = "admin123";
+
+}
